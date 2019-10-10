95bc59eb218d424c95c832c4456ecbef29e3ca37
Ivan Valkou <zloyfey@gmail.com>
2019-10-10 12:24:56 +0300

    Initial Commit

---
+++ NightscoutAPIClient/NightscoutAPIClient.h
@@ -0,0 +1,19 @@
+//
+//  NightscoutAPIClient.h
+//  NightscoutAPIClient
+//
+//  Created by Ivan Valkou on 10.10.2019.
+//  Copyright © 2019 Ivan Valkou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NightscoutAPIClient.
+FOUNDATION_EXPORT double NightscoutAPIClientVersionNumber;
+
+//! Project version string for NightscoutAPIClient.
+FOUNDATION_EXPORT const unsigned char NightscoutAPIClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NightscoutAPIClient/PublicHeader.h>
+
+
